8f0566e91f3e1dc27314f8b0e10f8bdda8323b47
Vedant Pandey <66998321+vp1001@users.noreply.github.com>
2024-02-27 13:44:44 +0530

    Add files via upload

---
+++ pdfchatOpenAI.py
@@ -0,0 +1,105 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("OPENAI_API_KEY")
+#genai.configure(api_key=os.getenv("OPENAI_API_KEY"))
+#os.environ["OPENAI_API_KEY"] = "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"
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(        
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+#vector_store = None
+
+def get_vector_store(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("OpenAI_VectorDatabase")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    
+    chain = load_qa_chain(OpenAI(), chain_type="stuff")
+
+    return chain
+
+
+
+def user_input(user_question):
+    embeddings = OpenAIEmbeddings()
+    
+    new_db = FAISS.load_local("OpenAI_VectorDatabase", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}, return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+
+
+def main():
+    st.set_page_config("Chat PDF OpenAI")
+    st.header("OpenAI")
+
+    user_question = st.text_input("Job Description")
+    user_prompt = st.text_input("Prompt")
+
+    if user_question and user_prompt:
+        user_input(user_question+". "+user_prompt)
+
+    with st.sidebar:
+        st.title("Sidebar")
+        pdf_docs = st.file_uploader("", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
