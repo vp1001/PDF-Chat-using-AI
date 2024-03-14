5cdb5cd9bb3ae0a12fd59db86e970b9f22c3d31f
Vedant Pandey <66998321+vp1001@users.noreply.github.com>
2024-03-14 13:29:48 +0530

    Update v2.0 pdfchatGemini.py

--- pdfchatGemini.py
+++ pdfchatGemini.py
@@ -34,7 +34,7 @@ def get_text_chunks(text):
 
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings, allow_dangerous_deserialization=True)
     vector_store.save_local("faiss_index")
 
 
